761d3dd8ca1c5a2e4ab8c0e9f485ee3087f3f1d9
ac-arifuzzaman <ac.arifuzzaman@gmail.com>
2021-10-31 22:38:59 +0600

    final commit

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         await client.connect();
         const database = client.db('curierService');
+        const myserviceCollection = database.collection('myservice');
         const servicesCollection = database.collection('services');
 
         // get all service
@@ -28,14 +29,21 @@ async function run() {
             res.send(service);
         });
 
+        app.get('/myservice', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const myservice = await cursor.toArray();
+            res.send(myservice);
+        });
+
         // get single service
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await servicesCollection.findOne(query);
             res.json(service)
-        })
-        // post api
+        });
+
+        // post api data
         app.post('/services', async (req, res) => {
             const service = req.body;
             console.log('hit the api', service)
@@ -43,6 +51,24 @@ async function run() {
             const result = await servicesCollection.insertOne(service)
             console.log(result)
             res.json(result)
+        });
+
+        app.post('/myservice', async (req, res) => {
+            const service = req.body;
+            console.log('hit the myservice', service)
+
+            const result = await myserviceCollection.insertOne(service);
+            console.log(result)
+            res.send(result);
+        });
+
+        // delete api
+
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
         })
     }
     finally {
